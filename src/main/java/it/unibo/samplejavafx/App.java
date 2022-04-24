89c17633bb5bc9e0434d65fac64c406dab32faa3
Andrea Zavatta <zavattaandrea@gmail.com>
2022-04-24 19:26:37 +0200

    setup project

--- src/main/java/it/unibo/samplejavafx/App.java
+++ src/main/java/it/unibo/samplejavafx/App.java
@@ -9,7 +9,7 @@ import javafx.application.Application;
 /** Main application entry-point. */
 
 public final class App {
-    private App() { }
+    public App() { }
 
     public static void main(final String[] args) {
         Application.launch(JavaFXApp.class, args);
