222a047fc64e69343dceec62d58754b383729303
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-04-25 11:58:16 +0200

    create Bishop piece

---
+++ src/main/java/pieces/Bishop.java
@@ -0,0 +1,36 @@
+package pieces;
+
+import java.util.List;
+
+import piece.utils.Position;
+import piece.utils.Color;
+import piece.utils.Name;
+/**
+ * 
+ *
+ */
+public class Bishop extends AbstractPiece {
+
+    Bishop(final Name name, final Position position, final Color color) {
+        super(name, position, color);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public boolean move() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public List<Position> getAllPossiblePositions() {
+        // TODO Auto-generated method stub
+        return List.of();
+    }
+
+    @Override
+    public int getValue() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+}
