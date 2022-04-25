45db36b202d9d03a2518484e8ff0e66cae6ef3db
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-04-25 11:57:23 +0200

    create Piece interface

--- src/main/java/piece/Piece.java
+++
@@ -1,5 +0,0 @@
-package piece;
-
-public interface Piece {
-
-}
---
+++ src/main/java/pieces/Piece.java
@@ -0,0 +1,45 @@
+package pieces;
+
+import java.util.List;
+
+import piece.utils.Color;
+import piece.utils.Name;
+import piece.utils.Position;
+
+/**
+ * A standard interface for a Piece object.
+ *
+ */
+public interface Piece {
+    /**
+     * 
+     * @return the name of the piece.
+     */
+    Name getName();
+    /**
+     * 
+     * @return true if the move is ok, false otherwise.
+     */
+    boolean move();
+    /**
+     * 
+     * @return a list of positions with all possible positions the piece can go.
+     */
+    List<Position> getAllPossiblePositions();
+    /**
+     * 
+     * @return the position of the piece.
+     */
+    Position getPosition();
+    /**
+     * 
+     * @return the color of the piece.
+     */
+    Color getColor();
+    /**
+     * 
+     * @return the value of the piece.
+     */
+    int getValue();
+
+}
