f4386a6ab0aed3f13516e69d384c813f04365308
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2022-04-25 16:48:34 +0200

    create chessboard interface

---
+++ src/main/java/board/Chessboard.java
@@ -0,0 +1,26 @@
+package board;
+
+import java.util.List;
+import piece.utils.Position;
+import pieces.Piece;
+
+    /**
+     * Interface for a simple chessboard. 
+     *
+     */
+public interface Chessboard {
+
+    /**
+     * 
+     * @return a list of all pieces on the board
+     */
+    List<Piece> getAllPieces();
+
+    /**
+     * 
+     * @param actualPos
+     * @param finalPos
+     * @return if the piece can do this move
+     */
+    boolean move(Position actualPos, Position finalPos);
+}
