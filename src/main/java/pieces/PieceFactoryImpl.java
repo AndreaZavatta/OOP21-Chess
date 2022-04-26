b1d809b41bfa798ae776cbd81cdef94f817a5db5
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-04-26 09:28:06 +0200

    create PieceFactoryImpl class

---
+++ src/main/java/pieces/PieceFactoryImpl.java
@@ -0,0 +1,17 @@
+package pieces;
+
+import piece.utils.Color;
+import piece.utils.Name;
+import piece.utils.Position;
+/**
+ * 
+ *
+ */
+public class PieceFactoryImpl implements PieceFactory {
+
+    @Override
+    public Piece createPiece(final Name name, final Position position, final Color color) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
