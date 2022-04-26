3f4a6c95e9724a8b3a165c46c29af9c6db0b903c
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-04-26 09:30:33 +0200

    update javadoc

--- src/main/java/piece/utils/Name.java
+++ src/main/java/piece/utils/Name.java
@@ -5,27 +5,27 @@ package piece.utils;
  */
 public enum Name {
     /**
-     * 
+     * Pawn piece name.
      */
     PAWN,
     /**
-     * 
+     * Queen piece name.
      */
     QUEEN,
     /**
-     * 
+     * King piece name.
      */
     KING,
     /**
-     * 
+     * Knight piece name.
      */
     KNIGHT,
     /**
-     * 
+     * Bishop piece name.
      */
     BISHOP,
     /**
-     * 
+     * Rook piece name.
      */
     ROOK
 }
