116b4250d457c6dd6ae60c4855307def3067fdbe
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-04-27 11:20:04 +0200

    implement PieceFactoryImpl class

--- src/main/java/pieces/PieceFactoryImpl.java
+++ src/main/java/pieces/PieceFactoryImpl.java
@@ -11,7 +11,20 @@ public class PieceFactoryImpl implements PieceFactory {
 
     @Override
     public Piece createPiece(final Name name, final Position position, final Color color) {
-        // TODO Auto-generated method stub
-        return null;
+        switch (name) {
+        case QUEEN: 
+            return new Queen(position, color);
+        case KNIGHT: 
+            return new Knight(position, color);
+        case PAWN: 
+            return new Pawn(position, color);
+        case KING: 
+            return new King(position, color);
+        case ROOK: 
+            return new Rook(position, color);
+        case BISHOP: 
+            return new Bishop(position, color);
+        default: return null;
+        }
     }
 }
