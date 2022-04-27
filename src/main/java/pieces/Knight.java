7fb767953393ad93531f0088569cd500d657ee0f
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-04-27 11:21:25 +0200

    implement Knight class

--- src/main/java/pieces/Knight.java
+++ src/main/java/pieces/Knight.java
@@ -12,8 +12,16 @@ import piece.utils.Name;
  */
 public final class Knight extends AbstractPiece {
 
-    Knight(final Name name, final Position position, final Color color) {
-        super(name, position, color);
+    private static final int KNIGHT_VALUE = 3;
+
+    /**
+     * A Knight piece constructor.
+     * 
+     * @param position the piece position.
+     * @param color the piece color.
+     */
+    protected Knight(final Position position, final Color color) {
+        super(Name.KNIGHT, position, color);
         // TODO Auto-generated constructor stub
     }
 
@@ -32,6 +40,6 @@ public final class Knight extends AbstractPiece {
     @Override
     public int getValue() {
         // TODO Auto-generated method stub
-        return 0;
+        return KNIGHT_VALUE;
     }
 }
