fb7c6c3dae896aae952617adad9a7ce63009480f
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-05-05 22:52:09 +0200

    add test

--- src/test/java/pieces/test/BishopTest.java
+++ src/test/java/pieces/test/BishopTest.java
@@ -1,7 +1,8 @@
 package pieces.test;
 
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,24 +22,73 @@ class BishopTest {
 
     private final PieceFactory factory = new PieceFactoryImpl();
     private final ChessboardFactory board = new ChessboardFactoryImpl();
-    private final List<Piece> list = new ArrayList<>();
 
     @Test
-    void test() {
+    void testNormalMovement() {
+        final List<Piece> list = new ArrayList<>();
+        final List<Position> l = List.of(new Position(5, 6), new Position(6, 7), new Position(3, 4),
+                new Position(2, 3), new Position(1, 2), new Position(0, 1), new Position(5, 4), 
+                new Position(6, 3), new Position(7, 2), new Position(3, 6), new Position(2, 7));
+        final Piece bishop = factory.createPiece(Name.BISHOP, new Position(4, 5), Color.WHITE);
+        list.add(bishop);
+        assertEquals(l, bishop.getAllPossiblePositions(board.createTestCB(list)));
+    }
+
+    @Test
+    void testWithAlliesAndNoPositionAvailable() {
+        final List<Piece> list = new ArrayList<>();
+        //        final List<Position> l = List.of(new Position(5, 6), new Position(6, 7), new Position(3, 4),
+        //                new Position(2, 3), new Position(1, 2), new Position(0, 1), new Position(5, 4), 
+        //                new Position(6, 3), new Position(7, 2), new Position(3, 6), new Position(2, 7));
+
+        final Piece bishop = factory.createPiece(Name.BISHOP, new Position(4, 5), Color.WHITE);
+
+        final Piece rook = factory.createPiece(Name.ROOK, new Position(5, 6), Color.WHITE);
+        final Piece rook1 = factory.createPiece(Name.ROOK, new Position(3, 4), Color.WHITE);
+        final Piece rook2 = factory.createPiece(Name.ROOK, new Position(5, 4), Color.WHITE);
+        final Piece rook3 = factory.createPiece(Name.ROOK, new Position(3, 6), Color.WHITE);
+        list.add(bishop);
+        list.add(rook3);
+        list.add(rook2);
+        list.add(rook1);
+        list.add(rook);
+
+        assertEquals(List.of(), bishop.getAllPossiblePositions(board.createTestCB(list)));
+        //        assertNotEquals(l, bishop.getAllPossiblePositions(board.createTestCB(list)));
+        assertTrue(bishop.getAllPossiblePositions(board.createTestCB(list)).isEmpty());
+    }
+
+    @Test
+    void testWithAlliesAndPositionAvailable() {
+        final List<Piece> list = new ArrayList<>();
+        final List<Position> l = List.of(new Position(5, 6), new Position(3, 4),
+                new Position(5, 4), new Position(3, 6));
+
+        final Piece bishop = factory.createPiece(Name.BISHOP, new Position(4, 5), Color.WHITE);
+
+        final Piece rook = factory.createPiece(Name.ROOK, new Position(6, 7), Color.WHITE);
+        final Piece rook1 = factory.createPiece(Name.ROOK, new Position(2, 7), Color.WHITE);
+        final Piece rook2 = factory.createPiece(Name.ROOK, new Position(2, 3), Color.WHITE);
+        final Piece rook3 = factory.createPiece(Name.ROOK, new Position(6, 3), Color.WHITE);
+        list.add(bishop);
+        list.add(rook3);
+        list.add(rook2);
+        list.add(rook1);
+        list.add(rook);
+        assertEquals(l, bishop.getAllPossiblePositions(board.createTestCB(list)));
+    }
+
+    @Test
+    void test2WithAlliesAndPositionAvailable() {
+        final List<Piece> list = new ArrayList<>();
+        final List<Position> l = List.of(new Position(1, 1));
+
         final Piece bishop = factory.createPiece(Name.BISHOP, new Position(0, 0), Color.WHITE);
-        final Piece king = factory.createPiece(Name.KING, new Position(2, 2), Color.WHITE);
-        final Piece knight = factory.createPiece(Name.KNIGHT, new Position(2, 0), Color.WHITE);
-        //final Piece queen = factory.createPiece(Name.QUEEN, new Position(4, 4), Color.BLACK);
-        //final Piece rook = factory.createPiece(Name.ROOK, new Position(7, 7), Color.BLACK);
-        final Piece pawn = factory.createPiece(Name.PAWN, new Position(1, 1), Color.BLACK);
-        //list.add(bishop);
-        list.add(king);
-        //list.add(queen);
-        list.add(pawn);
-        list.add(knight);
-        assertNotNull(bishop.getAllPossiblePositions(board.createTestCB(list)));
-        pawn.getAllPossiblePositions(board.createTestCB(list)).forEach(x -> System.out.println(x));
-        //System.out.println(bishop);
+
+        final Piece rook = factory.createPiece(Name.ROOK, new Position(2, 2), Color.WHITE);
+        list.add(bishop);
+        list.add(rook);
+        assertEquals(l, bishop.getAllPossiblePositions(board.createTestCB(list)));
     }
 
 }
