401d9fab6bbfab27fb5c7c1ed434df73b3773abd
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2022-05-06 17:13:09 +0200

    add new exception

---
+++ src/main/java/exceptions/PositionNotFoundException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+    /**
+     * 
+     * This exception is launched when you have to check
+     * if a position is in a list.
+     *
+     */
+public class PositionNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 3324858629217908080L;
+
+}
