a7ed8b15f95c337baeab05743d2aa8bc1a02e745
Andrea Zavatta <zavattaandrea@gmail.com>
2022-05-08 23:58:49 +0200

    create KingNotFoundException and add exception case

--- src/main/java/controls/ControlCheckImpl.java
+++ src/main/java/controls/ControlCheckImpl.java
@@ -4,12 +4,11 @@ import static piece.utils.Name.KING;
 import piece.utils.Color;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
 import piece.utils.Position;
 import board.Chessboard;
 import board.ChessboardFactory;
 import board.ChessboardFactoryImpl;
+import exceptions.KingNotFoundException;
 import pieces.Piece;
 
 /**
@@ -51,18 +50,15 @@ public class ControlCheckImpl implements ControlCheck {
     }
 
     private boolean canEatKing(final Chessboard chessboard, final Piece piece) {
-        final Optional<Position> pos = getEnemyKingPosition(chessboard, piece);
-        return pos.isPresent() && piece.getAllPossiblePositions(chessboard).contains(pos.get());
+        return piece.getAllPossiblePositions(chessboard)
+                .contains(getEnemyKingPosition(chessboard, piece));
     }
-    private Optional<Position> getEnemyKingPosition(final Chessboard chessboard, final Piece piece) {
+    private Position getEnemyKingPosition(final Chessboard chessboard, final Piece piece) {
         return chessboard.getAllPieces().stream()
                 .filter(x -> !x.getColor().equals(piece.getColor()))
                 .filter(x -> x.getName().equals(KING))
                 .findFirst()
-                .map(x -> Optional.of(x.getPosition()))
-                .orElse(Optional.empty());
+                .map(Piece::getPosition)
+                .orElseThrow(KingNotFoundException::new);
     }
-
-
-
 }
