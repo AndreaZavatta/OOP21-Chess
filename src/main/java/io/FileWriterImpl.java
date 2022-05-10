b13a61ca0fc5f6556dc57b95f58bbf0c09a6ee5b
Andrea Zavatta <zavattaandrea@gmail.com>
2022-05-10 18:32:24 +0200

    create FileWriter implementation

--- src/main/java/io/FileWriterImpl.java
+++ src/main/java/io/FileWriterImpl.java
@@ -1,7 +1,11 @@
 package io;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
 /**
  * 
  *
@@ -9,9 +13,27 @@ import java.io.Serializable;
  */
 public class FileWriterImpl<T extends Serializable> implements FileWriter<T> {
 
+    private final JsonSerializer jSerializer;
+    private final String fileName;
+    private final String fs = System.getProperty("file.separator");
+    private final String cd =  System.getProperty("user.dir");
+    /**
+     * 
+     * @param fileName
+     * @param className
+     */
+    public FileWriterImpl(final String fileName, final Class<T> className) {
+            this.fileName = fileName;
+            jSerializer = new JsonSerializerImpl<>(className);
+    }
+
     @Override
     public void writeFile(final T obj) throws IOException {
-        // TODO Auto-generated method stub
+            final String jsonString = jSerializer.serialize(obj);
+            final FileOutputStream file = new FileOutputStream(cd + fs + fileName);
+            try (var a = new BufferedWriter(new OutputStreamWriter(file, StandardCharsets.UTF_8))) {
+                a.write(jsonString);
+            }
     }
 
 }
