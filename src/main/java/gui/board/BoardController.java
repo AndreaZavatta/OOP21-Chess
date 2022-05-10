0b73823763a8001dd608d2b0fb38840c1492db09
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-05-10 23:08:14 +0200

    Update BoardController.java

--- src/main/java/gui/board/BoardController.java
+++ src/main/java/gui/board/BoardController.java
@@ -8,9 +8,13 @@ import board.Chessboard;
 import board.ChessboardFactory;
 import board.ChessboardFactoryImpl;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import piece.utils.Name;
 import piece.utils.Position;
 import pieces.Piece;
 
@@ -48,7 +52,11 @@ public class BoardController {
         final List<Piece> l =  board.getAllPieces();
         tiles.forEach(x -> {
             if (l.stream().map(y -> y.getPosition()).collect(Collectors.toList()).contains(x.getPosition())) {
-                getPos(x);
+                final Piece p = l.stream().filter(a -> a.getPosition().equals(x.getPosition())).findFirst().get();
+                if (p.getName().equals(Name.PAWN)) {
+                    final ImagePattern i = new ImagePattern(new Image("media/Unknown.jpg"));
+                    x.setFill(i);
+                }
             }
         });
     }
