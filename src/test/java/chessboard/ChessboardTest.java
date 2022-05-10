7c23ebe34ecf3ba34bbf64002ba342b95857a193
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2022-05-10 09:25:38 +0200

    add chessboard tests

--- src/test/java/chessboard/ChessboardTest.java
+++ src/test/java/chessboard/ChessboardTest.java
@@ -2,6 +2,7 @@ package chessboard;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
@@ -25,14 +26,27 @@ class ChessboardTest {
     private final ChessboardFactory chessboardFct = new ChessboardFactoryImpl();
 
     @Test
-    void test() throws PositionNotFoundException {
+    void creationChessboard() {
         final Chessboard board = chessboardFct.createNormalCB();
         assertEquals(32, board.getAllPieces().size());
+        assertEquals(16, board.getAllPieces().stream().filter(x -> x.getColor().equals(Color.WHITE)).count());
+        assertEquals(16, board.getAllPieces().stream().filter(x -> x.getColor().equals(Color.BLACK)).count());
+
+
+    }
+
+    @Test
+    void tryIllegalMove() throws PositionNotFoundException {
+        final Chessboard board = chessboardFct.createNormalCB();
+
         assertTrue(board.getPieceOnPosition(new Position(6, 0)).isPresent());
         assertFalse(board.getPieceOnPosition(new Position(4, 0)).isPresent());
         board.move(new Position(6, 0), new Position(4, 0));
         assertFalse(board.getPieceOnPosition(new Position(6, 0)).isPresent());
         assertTrue(board.getPieceOnPosition(new Position(4, 0)).isPresent());
+
+        assertThrows(PositionNotFoundException.class, 
+                () -> board.move(new Position(4, 0), new Position(2, 0)));
     }
 
 }
