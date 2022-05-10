8e21e0a0f572cc2f0f4287a96552b908c2f08f71
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-05-10 19:44:36 +0200

    Create Tile.java

---
+++ src/main/java/gui/board/Tile.java
@@ -0,0 +1,28 @@
+package gui.board;
+
+import javafx.scene.shape.Rectangle;
+import piece.utils.Position;
+
+/**
+ * 
+ *
+ */
+public class Tile  extends Rectangle {
+
+    private final Position position;
+
+    Tile(final int x, final int y) {
+        setWidth(BoardController.TILE_SIZE);
+        setHeight(BoardController.TILE_SIZE);
+        this.position = new Position(x, y);
+    }
+
+    /**
+     * 
+     * @return the position
+     */
+    public Position getPosition() {
+        return this.position;
+    }
+
+}
