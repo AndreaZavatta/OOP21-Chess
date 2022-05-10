1a48cbadf192d183370b49f1e09b04acae385b0f
Andrea Zavatta <zavattaandrea@gmail.com>
2022-05-10 18:32:39 +0200

    create FileReader implementation

--- src/main/java/io/FileReaderImpl.java
+++ src/main/java/io/FileReaderImpl.java
@@ -1,18 +1,48 @@
 package io;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
 /**
  * 
  * 
  * @param <T>
  */
 public class FileReaderImpl<T extends Serializable> implements FileReader<T> {
+    private final JsonDeserializer<T> jdeserializer;
+    private final String fileName;
+    private final String fs = System.getProperty("file.separator");
+    private final String cd =  System.getProperty("user.dir");
+
+    /**
+     * 
+     * @param fileName
+     * @param className
+     */
+    public FileReaderImpl(final String fileName, final Class<T> className) {
+            this.fileName = fileName;
+            jdeserializer = new JsonDeserializerImpl<>(className);
+    }
+
 
     @Override
     public T readFile() throws IOException, ClassNotFoundException {
-        // TODO Auto-generated method stub
-        return null;
+            final FileInputStream file = new FileInputStream(cd + fs + fileName);
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(file, StandardCharsets.UTF_8))) {
+                    return jdeserializer.deserialize(composeString(in));
+            }
+    }
+
+
+    private String composeString(final BufferedReader in) throws IOException {
+            final StringBuilder str = new StringBuilder();
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                str.append(line);
+            }
+            return str.toString();
     }
 
 }
