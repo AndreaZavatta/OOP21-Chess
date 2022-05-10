74365ae9dc9301b9ce4680eebce8402c95a985f9
Andrea Zavatta <zavattaandrea@gmail.com>
2022-05-10 18:30:58 +0200

    create JsonDeserializer implementation

--- src/main/java/io/JsonDeserializerImpl.java
+++ src/main/java/io/JsonDeserializerImpl.java
@@ -1,17 +1,25 @@
 package io;
 
 import java.io.Serializable;
+import java.util.Objects;
+import com.google.gson.Gson;
 /**
  * 
  *
  * @param <T>
  */
 public class JsonDeserializerImpl<T extends Serializable> implements JsonDeserializer<T> {
-
+    private Gson gson = new Gson();
+    private final Class<T> className;
+    /**
+     * 
+     * @param className
+     */
+    public JsonDeserializerImpl(final Class<T> className) {
+            this.className = Objects.requireNonNull(className);
+    }
     @Override
     public T deserialize(final String str) {
-        // TODO Auto-generated method stub
-        return null;
+            return gson.fromJson(Objects.requireNonNull(str), className);
     }
-
 }
