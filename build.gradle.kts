97dd72057a2465338bbc51c224158ebbfdd98d0d
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-05-11 09:39:13 +0200

    add gson

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,9 @@ dependencies {
     // JUnit API and testing engine
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
-    compile group: 'com.google.code.gson', name: 'gson', version: '2.8.9'
+    //compile group: 'com.google.code.gson', name: 'gson', version: '2.8.9'
+    //api("com.google.code.gson:gson:2.8.6")
+    implementation("com.google.code.gson:gson:2.9.0")
 }
 
 tasks.withType<JavaCompile> {
