3b915f6141a87849d8ecc1bf2909f50310cf47fa
Angela Speranza <angela.speranza@studio.unibo.it>
2022-05-11 11:04:33 +0200

    tutorial implementation

--- src/main/java/controller/TutorialController.java
+++ src/main/java/controller/TutorialController.java
@@ -8,7 +8,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 /**
@@ -18,10 +17,6 @@ import javafx.stage.Stage;
  */
 
 public class TutorialController {
-
-    @FXML
-    private Button slider;
-    private Button slider2;
 
     @FXML
     void openSlide(final ActionEvent event) {
@@ -54,9 +49,121 @@ public class TutorialController {
             System.out.println(e);
         }
     }
+//
+//    @FXML
+//    void goBack(final ActionEvent event) {
+//
+//    }
 
     @FXML
-    void show(final MouseEvent event) {
-        slider.setVisible(true);
+    void showPiece(final ActionEvent event) {
+        Button caller = (Button)event.getSource();
+        System.out.println(caller.getId());
+//        try {
+//            FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/tutorial/"+piece+".fxml"));
+//            Parent root = (Parent) loader.load();
+//            Stage stage = new Stage();
+//            stage.setTitle("Pieces");
+//            stage.setScene(new Scene(root));
+//            stage.show();
+//            ((Node) (event.getSource())).getScene().getWindow().hide();
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        }
+    }
+    
+
+  void showBishop(final ActionEvent event) {
+      showPiece("Bishop", event);
+  }
+    
+    @FXML
+    void showBishop(final ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/tutorial/Bishop.fxml"));
+            Parent root = (Parent) loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Stats");
+            stage.setScene(new Scene(root));
+            stage.show();
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @FXML
+    void showKing(final ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/tutorial/King.fxml"));
+            Parent root = (Parent) loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Stats");
+            stage.setScene(new Scene(root));
+            stage.show();
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @FXML
+    void showKnight(final ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/tutorial/Knight.fxml"));
+            Parent root = (Parent) loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Stats");
+            stage.setScene(new Scene(root));
+            stage.show();
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @FXML
+    void showPawn(final ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/tutorial/Pawn.fxml"));
+            Parent root = (Parent) loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Stats");
+            stage.setScene(new Scene(root));
+            stage.show();
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @FXML
+    void showQueen(final ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/tutorial/Queen.fxml"));
+            Parent root = (Parent) loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Stats");
+            stage.setScene(new Scene(root));
+            stage.show();
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @FXML
+    void showRook(final ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/tutorial/Rook.fxml"));
+            Parent root = (Parent) loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Stats");
+            stage.setScene(new Scene(root));
+            stage.show();
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
