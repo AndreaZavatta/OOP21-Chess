404a45b6cafbade57c872037e5af800343f75f2f
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-05-13 22:17:49 +0200

    Update BoardController.java

--- src/main/java/gui/board/BoardController.java
+++ src/main/java/gui/board/BoardController.java
@@ -73,19 +73,16 @@ public class BoardController {
             if (l.stream().map(y -> y.getPosition()).collect(Collectors.toList()).contains(pos)) {
                 final Piece p = l.stream().filter(a -> a.getPosition().equals(pos)).findFirst().get();
                 if (p.getName().equals(Name.PAWN)) {
-                    //final ImagePattern i = new ImagePattern(new Image("/pieces/images/blackPawn.png"));
-                    //final ImageView im = new ImageView("/pieces/images/blackPawn.png");
                     final Image im = new Image("/pieces/images/blackPawn.png");
-                    final Circle c = new Circle();
+                    final GuiPiece c = new GuiPiece(p.getPosition().getX(), p.getPosition().getY(), new Circle());
                     c.setFill(new ImagePattern(im));
-                    c.setStroke(Color.BLACK);
+                    //c.setStroke(Color.BLACK);
                     c.prefHeight(10);
                     c.prefWidth(10);
-                    c.setOnMouseClicked(e -> System.out.println("a"));
                     c.setOnMousePressed(event -> pressed(event, c));
                     c.setOnMouseDragged(event -> dragged(event, c));
                     c.setOnMouseReleased(event -> released(event, c));
-                    c.setRadius(30);
+                    c.setRadius(10);
                     pane.add(c, pos.getX(), pos.getY());
                 }
             }
@@ -97,23 +94,22 @@ public class BoardController {
         r.setFill(Color.BEIGE);
     }
 
-    private void pressed(final MouseEvent event, final Circle c) {
+    private void pressed(final MouseEvent event, final GuiPiece c) {
         c.setFill(Color.DARKGOLDENROD);
-        System.out.println(c.getCenterX());
+        System.out.println(c.getPosition());
     }
 
-    private void dragged(final MouseEvent event, final Circle c) {
-        c.setCenterX(c.getCenterX() + event.getX());
-        c.setCenterY(c.getCenterY() + event.getY());
-        //p.draw();
+    private void dragged(final MouseEvent event, final GuiPiece c) {
+        c.setX(c.getX() + event.getX());
+        c.setX(c.getY() + event.getY());
+        c.draw();
     }
 
-    private void released(final MouseEvent event, final Circle p) {
-        final int gridx = (int) p.getCenterX() / TILE_SIZE;
-        final int gridy = (int) p.getCenterY() / TILE_SIZE;
-        //grid[gridx][gridy].setFill(Color.CRIMSON);
-        p.setCenterX(TILE_SIZE / 2 + TILE_SIZE * gridx);
-        p.setCenterY(TILE_SIZE / 2 + TILE_SIZE * gridy);
-        //p.draw();
+    private void released(final MouseEvent event, final GuiPiece p) {
+        final int gridx = (int) p.getX() / TILE_SIZE;
+        final int gridy = (int) p.getY() / TILE_SIZE;
+        p.setX(TILE_SIZE / 2 + TILE_SIZE * gridx);
+        p.setY(TILE_SIZE / 2 + TILE_SIZE * gridy);
+        p.draw();
     }
 }
