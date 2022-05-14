1620d313278878f4ca68fef94d3353f4eda15eea
Andrea Zavatta <zavattaandrea@gmail.com>
2022-05-14 14:31:49 +0200

    update javadoc in Exception class

--- src/main/java/exceptions/IllegalMoveException.java
+++ src/main/java/exceptions/IllegalMoveException.java
@@ -1,5 +1,9 @@
 package exceptions;
-
+/**
+ * this exception is thrown when trying to rebuild a move via the movebuilder.
+ * but this is not a valid move
+ *
+ */
 public class IllegalMoveException extends Exception {
 
     private static final long serialVersionUID = 1L;
