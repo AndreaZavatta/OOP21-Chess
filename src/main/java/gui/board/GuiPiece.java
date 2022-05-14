2f1bdc3765c76fc6d930ca9017d19d68be1ef2a7
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-05-14 10:37:49 +0200

    add methods

--- src/main/java/gui/board/GuiPiece.java
+++ src/main/java/gui/board/GuiPiece.java
@@ -1,5 +1,7 @@
 package gui.board;
 
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 /**
  * 
@@ -15,12 +17,18 @@ public class GuiPiece extends Circle {
      * 
      * @param x the x value
      * @param y the y value
-     * @param circle the circle
+     * @param c the circle
      */
-    protected GuiPiece(final int x, final int y, final Circle circle) {
-        this.circle = circle;
+    protected GuiPiece(final int x, final int y, final Circle c) {
+        this.circle = c;
         this.x = x;
         this.y = y;
+        this.prefHeight(10);
+        this.prefWidth(10);
+        this.setOnMousePressed(event -> pressed(this));
+        this.setOnMouseDragged(event -> dragged(event, this));
+        this.setOnMouseReleased(event -> released(this));
+        this.setRadius(10);
     }
     /**
      * 
@@ -57,10 +65,31 @@ public class GuiPiece extends Circle {
     public double getY() {
         return y;
     }
-    
+    /**
+     * 
+     */
     public void draw() {
         circle.setRadius(30);
         circle.setTranslateX(x);
         circle.setTranslateY(y);
+    }
+
+    private void pressed(final GuiPiece c) {
+        //c.setFill(Color.DARKGOLDENROD);
+        System.out.println(c.getPosition());
+    }
+
+    private void dragged(final MouseEvent event, final GuiPiece c) {
+        c.setX(c.getX() + event.getX());
+        c.setX(c.getY() + event.getY());
+        //c.draw();
+    }
+
+    private void released(final GuiPiece p) {
+        final int gridx = (int) p.getX() / BoardController.TILE_SIZE;
+        final int gridy = (int) p.getY() / BoardController.TILE_SIZE;
+        p.setX(BoardController.TILE_SIZE / 2 + BoardController.TILE_SIZE * gridx);
+        p.setY(BoardController.TILE_SIZE / 2 + BoardController.TILE_SIZE * gridy);
+        //p.draw();
     }
 }
