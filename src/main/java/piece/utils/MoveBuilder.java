85f02cd653a6c484346ecb2483a242e1f1dc5ff4
Andrea Zavatta <zavattaandrea@gmail.com>
2022-05-17 01:03:08 +0200

    remove getter from MoveBuilder and create the function to set rank and file fields

--- src/main/java/piece/utils/MoveBuilder.java
+++ src/main/java/piece/utils/MoveBuilder.java
@@ -1,8 +1,12 @@
 package piece.utils;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import board.Chessboard;
+import board.ControlCheck;
+import board.ControlCheckImpl;
 import exceptions.IllegalMoveException;
 import pieces.Piece;
 
@@ -11,19 +15,19 @@ import pieces.Piece;
  *
  */
 public class MoveBuilder implements Move {
+    private final ControlCheck controls = new ControlCheckImpl();
     private Optional<Piece> piece = Optional.empty();
     private Optional<Position> destination = Optional.empty();
-    private boolean isPromotion = false;
-    private boolean isDrawOffer = false;
-    private boolean isCheck = false;
-    private boolean isCheckmate = false;
-    private boolean isCapture = false;
-    private boolean isKingsideCastling = false;
-    private boolean isQueensideCastling = false;
-    @Override
-    public Optional<Piece> getPiece() {
-        return piece;
-    }
+    private Optional<Piece> promotion = Optional.empty();
+    private boolean drawOffer = false;
+    private boolean check = false;
+    private boolean checkmate = false;
+    private boolean capture = false;
+    private boolean kingsideCastling = false;
+    private boolean queenSideCastling = false;
+    private boolean file = false;
+    private boolean rank = false;
+
 
     @Override
     public Move setPiece(final Piece piece) {
@@ -32,103 +36,123 @@ public class MoveBuilder implements Move {
     }
 
     @Override
-    public Optional<Position> getDestination() {
-        return destination;
-    }
-    @Override
     public Move setDestination(final Position destination) {
         this.destination = Optional.ofNullable(destination);
         return this;
     }
-    @Override
-    public boolean isCapture() {
-        return isCapture;
-    }
+
 
     @Override
     public Move setCapture() {
-        this.isCapture = true;
+        this.capture = true;
         return this;
     }
 
-    @Override
-    public boolean isKingsideCastling() {
-        return isKingsideCastling;
-    }
+
 
     @Override
     public Move setKingsideCastling() {
-        this.isKingsideCastling = true;
+        this.kingsideCastling = true;
         return this;
     }
 
-    @Override
-    public boolean isQueensideCastling() {
-        return isQueensideCastling;
-    }
 
     @Override
     public Move setQueensideCastling() {
-        this.isQueensideCastling = true;
+        queenSideCastling = true;
         return this;
     }
 
-    @Override
-    public boolean isPromotion() {
-        return isPromotion;
-    }
 
     @Override
-    public Move setPromotion() {
-        this.isPromotion = true;
+    public Move setPromotion(final Piece piece) {
+        this.promotion = Optional.ofNullable(piece);
         return this;
     }
 
-    @Override
-    public boolean isDrawOffer() {
-        return isDrawOffer;
-    }
 
     @Override
     public Move setDrawOffer() {
-        this.isDrawOffer = true;
+        this.drawOffer = true;
         return this;
     }
 
-    @Override
-    public boolean isCheck() {
-        return isCheck;
-    }
 
     @Override
     public Move setCheck() {
-        this.isCheck = true;
+        this.check = true;
+        return this;
+    }
+
+
+    @Override
+    public Move setCheckmate() {
+        this.checkmate = true;
         return this;
     }
 
     @Override
-    public boolean isCheckmate() {
-        return isCheckmate;
+    public Move setRank() {
+        rank = true;
+        return this;
     }
 
     @Override
-    public Move setCheckmate() {
-        this.isCheckmate = true;
+    public Move setFile() {
+        file = true;
         return this;
     }
 
     @Override
     public Move build(final Chessboard chessboard) throws IllegalMoveException {
-        if (this.piece.isEmpty() || this.getDestination().isEmpty()) {
-            throw new IllegalMoveException();
-        }
+        setRankAndFile(chessboard);
         return this;
+    }
+    /*
+     * function that check if there is the same piece in the same row or column that can go in the same destination
+     * update the rank and file field
+     * */
+    private void setRankAndFile(final Chessboard chessboard) {
+        List<Piece> pieces = piecesSameTypeCanGoDestPos(chessboard);
+        findPiecesSameRank(pieces).ifPresent(x -> setRank());
+        findPiecesSameFile(pieces).ifPresent(x -> setFile());
+    }
+
+    private Optional<Piece> findPiecesSameRank(final List<Piece> pieces) {
+        return pieces.stream()
+        .filter(x -> (x.getPosition().getX() == (piece.get().getPosition().getX())))
+        .findAny();
+    }
+    private Optional<Piece> findPiecesSameFile(final List<Piece> pieces) {
+        return pieces.stream()
+        .filter(x -> (x.getPosition().getY() == (piece.get().getPosition().getY())))
+        .findAny();
+    }
+
+    private List<Piece> piecesSameTypeCanGoDestPos(final Chessboard chessboard) {
+        return getPiecesSameType(chessboard)
+                .stream()
+                .filter(x -> controls.controlledMoves(chessboard, x).contains(destination.get()))
+                .collect(Collectors.toList());
+    }
+
+    private List<Piece> getPiecesSameType(final Chessboard chessboard) {
+        return chessboard.getAllPieces().stream()
+        .filter(x -> x.getName().equals(piece.get().getName()))
+        .filter(x -> !x.equals(piece.get())).collect(Collectors.toList());
     }
     /**
      * 
      * @return the string representation of the move
      */
     public String toStirng() {
+        if (drawOffer) {
+            return "(=)";
+        } else if (kingsideCastling) {
+            return "0-0";
+        } else if (queenSideCastling) {
+            return "0-0-0";
+        }
         return "";
     }
 
