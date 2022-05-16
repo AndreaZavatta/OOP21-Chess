bc811ecc1ad6e5c75d08dd39baafcef823775d70
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-05-16 15:18:48 +0200

    add test

--- src/test/java/pieces/test/PawnTest.java
+++ src/test/java/pieces/test/PawnTest.java
@@ -88,4 +88,20 @@ class PawnTest {
         assertTrue(pawn.isMoved());
     }
 
+    @Test
+    void testGoFowardTwoTilesAndCanEatLeftAndRightWhitePawn() {
+        final List<Piece> list = new ArrayList<>();
+        final List<Position> l = List.of(new Position(4, 4), new Position(6, 4), new Position(5, 4), new Position(5, 3));
+        final Piece pawn = factory.createPiece(Name.PAWN, new Position(5, 5), Side.WHITE);
+        final Piece bishop = factory.createPiece(Name.BISHOP, new Position(4, 4), Side.BLACK);
+        final Piece bishop1 = factory.createPiece(Name.BISHOP, new Position(6, 4), Side.BLACK);
+        list.add(pawn);
+        list.add(bishop1);
+        list.add(bishop);
+        assertEquals(l, pawn.getAllPossiblePositions(board.createTestCB(list)));
+        assertFalse(pawn.isMoved());
+        pawn.setIsMoved();
+        assertTrue(pawn.isMoved());
+    }
+
 }
