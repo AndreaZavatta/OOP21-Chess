3e21721920d6f914acadadfebb0fa81d33b46d64
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-05-16 17:05:04 +0200

    Update GuiPiece.java

--- src/main/java/gui/board/GuiPiece.java
+++ src/main/java/gui/board/GuiPiece.java
@@ -1,13 +1,11 @@
 package gui.board;
 
-import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 /**
  * 
  *
  */
-public class GuiPiece extends Circle {
+public class GuiPiece{
 
     private double x;
     private double y;
@@ -23,12 +21,6 @@ public class GuiPiece extends Circle {
         this.circle = c;
         this.x = x;
         this.y = y;
-        this.prefHeight(10);
-        this.prefWidth(10);
-        this.setOnMousePressed(event -> pressed(this));
-        this.setOnMouseDragged(event -> dragged(event, this));
-        this.setOnMouseReleased(event -> released(this));
-        this.setRadius(30);
     }
     /**
      * 
@@ -64,32 +56,5 @@ public class GuiPiece extends Circle {
      */
     public double getY() {
         return y;
-    }
-    /**
-     * 
-     */
-    public void draw() {
-        circle.setRadius(30);
-        circle.setTranslateX(x);
-        circle.setTranslateY(y);
-    }
-
-    private void pressed(final GuiPiece c) {
-        c.setFill(Color.DARKGOLDENROD);
-        System.out.println(c.getPosition());
-    }
-
-    private void dragged(final MouseEvent event, final GuiPiece c) {
-        c.setX(c.getX() + event.getX());
-        c.setX(c.getY() + event.getY());
-        c.draw();
-    }
-
-    private void released(final GuiPiece p) {
-        final int gridx = (int) p.getX() / BoardController.TILE_SIZE;
-        final int gridy = (int) p.getY() / BoardController.TILE_SIZE;
-        p.setX(BoardController.TILE_SIZE / 2 + BoardController.TILE_SIZE * gridx);
-        p.setY(BoardController.TILE_SIZE / 2 + BoardController.TILE_SIZE * gridy);
-        p.draw();
     }
 }
