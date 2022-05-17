77264b843aa208abb4dcd048033df99f25e976dd
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-05-17 12:02:39 +0200

    Create QueenTest.java

---
+++ src/test/java/pieces/test/QueenTest.java
@@ -0,0 +1,14 @@
+package pieces.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class QueenTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
