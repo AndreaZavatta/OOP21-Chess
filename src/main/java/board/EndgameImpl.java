9dbf3bef7b18d80e58b7820b0e453206a403fb15
Angela Speranza <angela.speranza@studio.unibo.it>
2022-05-17 10:12:55 +0200

    Implementation of EndGame interface's methods

--- src/main/java/board/EndgameImpl.java
+++ src/main/java/board/EndgameImpl.java
@@ -2,31 +2,88 @@ package board;
 
 import piece.utils.Side;
 import java.util.List;
-import piece.utils.Position;
-import pieces.King;
+import java.util.stream.Collectors;
 import pieces.Piece;
-import board.*;
+
 
 /**
  * 
  * This Interface models and handles all the possible ways a chess game can end with.
  *
  */
-public class EndgameImpl {
+public class EndGameImpl implements EndGame {
 
-    private boolean gameOver = false;
+    private final boolean gameOver;
+    private final ControlCheck controls = new ControlCheckImpl(); 
 
-    //    public boolean isGameOver() {
-    //        return gameOver;
-    //    };
-    
-    private void checkEnemyPath() {
-        
+    /**
+     * 
+     * @param gameOver
+     */
+    public EndGameImpl(final boolean gameOver) {
+        super();
+        this.gameOver = gameOver;
     }
 
-    public boolean isCheckmate() {
-        
-        return false;
+    /**
+     * 
+     * @return
+     */
+    public boolean isGameOver() {
+        return gameOver;
+    };
+
+    /**
+     * 
+     * @param side
+     * @param chessboard
+     * @return
+     */
+    public boolean isCheckmate(final Side side, final Chessboard chessboard) {
+
+        final List<Piece> attackedColor = getAttackedSide(side, chessboard);
+
+        if (controlCheck(side, chessboard, controls)) {
+            for (final Piece shield : attackedColor) {
+                if (!checkShieldPosition(chessboard, controls, shield)) {
+                   return gameOver;
+                } 
+            }
+        }
+        return !gameOver;
+    }
+
+    /**
+     * 
+     * @param side
+     * @param chessboard
+     * @return
+     */
+    public boolean isStalemate(final Side side, final Chessboard chessboard) {
+
+        final List<Piece> attackedColor = getAttackedSide(side, chessboard);
+
+        for (final Piece piece : attackedColor) {
+            if (!checkShieldPosition(chessboard, controls, piece)) {
+                return gameOver;
+            }
+        }
+        return !gameOver;
+    }
+
+
+    private List<Piece> getAttackedSide(final Side side, final Chessboard chessboard) {
+        return chessboard.getAllPieces().stream()
+                .filter(x -> x.getColor().equals(side))
+                .collect(Collectors.toList());
+    }
+
+    private boolean checkShieldPosition(final Chessboard chessboard, final ControlCheck controls, final Piece shield) {
+        return controls.controlledMoves(chessboard, shield).isEmpty(); 
+    }
+
+    private boolean controlCheck(final Side side, final Chessboard chessboard, final ControlCheck controls) {
+        return controls.isInCheck(chessboard, side);
     }
 
 }
