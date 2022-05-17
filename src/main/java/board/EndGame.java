dd11a509b66353669bc533a383d35884c89fae4c
Angela Speranza <angela.speranza@studio.unibo.it>
2022-05-17 10:12:23 +0200

    implementation of the endgame interface

---
+++ src/main/java/board/EndGame.java
@@ -0,0 +1,35 @@
+package board;
+
+import piece.utils.Side;
+
+
+/**
+ * 
+ * 
+ *
+ */
+public interface EndGame {
+
+    /**
+     * 
+     * @return
+     */
+    boolean isGameOver();
+
+    /**
+     * 
+     * @param side
+     * @param chessboard
+     * @return
+     */
+    boolean isCheckmate(Side side, Chessboard chessboard);
+
+    /**
+     * 
+     * @param side
+     * @param chessboard
+     * @return
+     */
+    boolean isStalemate(Side side, Chessboard chessboard);
+
+}
