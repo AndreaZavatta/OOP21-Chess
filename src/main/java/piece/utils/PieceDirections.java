0b885d17462f8c3e17ad5cdf51c84264648387ad
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-05-17 11:21:07 +0200

    create new enum for piece's directions

---
+++ src/main/java/piece/utils/PieceDirections.java
@@ -0,0 +1,74 @@
+package piece.utils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * This enum contains the standard directions a piece can go to.
+ *
+ */
+public enum PieceDirections {
+    /**
+     * The pawn directions.
+     */
+    PAWN_DIR {
+        @Override
+        public List<Position> directions() {
+            // TODO Auto-generated method stub
+            return List.of(new Position(+1, +1), new Position(-1, +1));
+        }
+    },
+    /**
+     * The king directions.
+     */
+    KING_DIR {
+        @Override
+        public List<Position> directions() {
+            // TODO Auto-generated method stub
+            return Stream.concat(ROOK_DIR.directions().stream(),
+                    BISHOP_DIR.directions().stream()).collect(Collectors.toList());
+        }
+    },
+    /**
+     * The rook directions.
+     */
+    ROOK_DIR {
+        @Override
+        public List<Position> directions() {
+            return List.of(new Position(+1, 0), new Position(-1, 0), 
+                    new Position(0, +1), new Position(0, -1));
+        }
+    },
+    /**
+     * The bishop directions.
+     */
+    BISHOP_DIR {
+        @Override
+        public List<Position> directions() {
+            // TODO Auto-generated method stub
+            return List.of(new Position(+1, +1), new Position(-1, -1),
+                    new Position(+1, -1), new Position(-1, +1));
+        }
+    },
+    /**
+     * The knight directions.
+     */
+    KNIGHT_DIR {
+        @Override
+        public List<Position> directions() {
+            // TODO Auto-generated method stub
+            return List.of(new Position(-1, -2), new Position(+1, -2),
+                    new Position(+2, -1), new Position(+2, +1),
+                    new Position(-1, +2), new Position(+1, +2),
+                    new Position(-2, -1), new Position(-2, +1));
+        }
+    };
+
+    /**
+     * 
+     * @return the directions a piece can go to.
+     */
+    public abstract List<Position> directions();
+
+}
