1627b95cc56ef3781e593b2de80c43aaa2f7d3df
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2022-05-20 10:39:34 +0200

    create PieceNotFoundException

---
+++ src/main/java/exceptions/PieceNotFoundException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+/**
+ * 
+ * This exceptions is thrown when a position 
+ * is selected where there is no piece on it.
+ *
+ */
+public class PieceNotFoundException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+}
