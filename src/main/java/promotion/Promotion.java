59db95eb215fe8137ea9910c2961d29145aec613
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-05-22 13:49:56 +0200

    update javadoc

--- src/main/java/promotion/Promotion.java
+++ src/main/java/promotion/Promotion.java
@@ -5,21 +5,27 @@ import java.util.Optional;
 
 import piece.utils.Name;
 import pieces.Piece;
-
+/**
+ * An interface for the pawn's promotion.
+ *
+ */
 public interface Promotion {
 
     /**
+     * This method checks if there is a pawn on the highest or lowest row 
+     * of the board.
      * 
-     * @param pieceList
-     * @return a
+     * @param pieceList the current list of pieces
+     * @return an Optional of piece
      */
     Optional<Piece> checkForPromotion(List<Piece> pieceList);
 
     /**
+     * This method is used to create a new piece. 
      * 
-     * @param name
-     * @param piece
-     * @return a
+     * @param name the name of the piece you will create
+     * @param piece the old piece you get the position and side from
+     * @return a new piece with the position and side from the old piece
      */
     Piece changePiece(Name name, Piece piece);
 
