0460028f18a6fbc95cd5988f58cc3a391d8860bd
Andrea Zavatta <zavattaandrea@gmail.com>
2022-05-24 15:51:57 +0200

    add tests

--- src/test/java/chessparsers/test/FenBuilderTest.java
+++ src/test/java/chessparsers/test/FenBuilderTest.java
@@ -1,7 +1,9 @@
 package chessparsers.test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static piece.utils.Side.WHITE;
+import static piece.utils.Name.*;
+import static piece.utils.Side.*;
+import static piece.utils.Position.createNewPosition;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +13,7 @@ import board.ChessboardFactory;
 import board.ChessboardFactoryImpl;
 import chess.parsers.Fen;
 import chess.parsers.FenBuilder;
+import piece.utils.Position;
 import pieces.Piece;
 import pieces.PieceFactory;
 import pieces.PieceFactoryImpl;
@@ -28,5 +31,27 @@ class FenBuilderTest {
                 .fullMoveNumber(1)
                 .activeColor(WHITE)
                 .build(boardFactory.createNormalCB()));
+    }
+    @Test
+    void testFirstPosition() {
+        List<Piece> list = List.of(pieceFact.createPiece(ROOK, createNewPosition("a8"), BLACK),
+                                   pieceFact.createPiece(KING, createNewPosition("e8"), BLACK),
+                                   pieceFact.createPiece(BISHOP, createNewPosition("h8"), BLACK),
+                                   pieceFact.createPiece(ROOK, createNewPosition("a1"), WHITE),
+                                   pieceFact.createPiece(KING, createNewPosition("d2"), WHITE));
+        var board = boardFactory.createTestCB(list);
+        System.out.println(fenBuilder.fullMoveNumber(1)
+                .activeColor(WHITE)
+                .blackCastledKingside()
+                .whiteCastledKingside()
+                .whiteCastledQueenside()
+                .build(board));
+        assertEquals("r3k2b/8/8/8/8/8/3K4/R7 w q - 0 1", 
+                fenBuilder.fullMoveNumber(1)
+                .activeColor(WHITE)
+                .blackCastledKingside()
+                .whiteCastledKingside()
+                .whiteCastledQueenside()
+                .build(board));
     }
 }
