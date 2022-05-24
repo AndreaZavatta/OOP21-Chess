66b8c69bd5452d4c85a18be31df94ab176bd5bf3
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-05-24 14:20:27 +0200

    refactor methods

--- src/main/java/move/BasicMovesImpl.java
+++ src/main/java/move/BasicMovesImpl.java
@@ -1,9 +1,8 @@
 package move;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import board.Chessboard;
 import piece.utils.ControlsUtility;
@@ -18,34 +17,33 @@ public class BasicMovesImpl implements BasicMoves {
 
     @Override
     public List<Position> doubleIteration(final PieceDirections directions, final Chessboard board, final Piece piece) {
-//        directions.directions().stream()
-//            .map
-//            .map(p -> ControlsUtility.getNewPosition(piece, p, 1)); takewhile
-        final List<Position> list = new ArrayList<>();
-        for (final var pos : directions.directions()) {
-            for (int i = 1; i < 8; i++) {
-                final Position p = ControlsUtility.getNewPosition(piece, pos, i);
-                if (ControlsUtility.checkPiece(piece, p, board)) {
-                    if (ControlsUtility.checkEnemy(piece, p, board)) {
-                        list.add(p);
-                    } 
-                    break;
-                } else if (ControlsUtility.checkPosition(piece, p, board)) {
-                    list.add(p);
-                } else {
-                    break;
-                }
-            }
-        }
-        return Collections.unmodifiableList(list);
+        return directions.directions().stream()
+                .flatMap(d -> 
+                    IntStream.range(1, 8)
+                    .takeWhile(i -> isMovementValid(piece, i, d, board))
+                    .mapToObj(i -> ControlsUtility.getNewPosition(piece, d, i)))
+                .collect(Collectors.toUnmodifiableList());
     }
 
     @Override
     public List<Position> singleIteration(final PieceDirections directions, final Chessboard board, final Piece piece) {
-         return directions.directions().stream()
-            .map(p -> ControlsUtility.getNewPosition(piece, p, 1))
-            .filter(p -> ControlsUtility.checkPosition(piece, p, board))
-            .filter(p -> !ControlsUtility.checkPiece(piece, p, board) || ControlsUtility.checkEnemy(piece, p, board))
-            .collect(Collectors.toUnmodifiableList());
+        return directions.directions().stream()
+                .map(p -> ControlsUtility.getNewPosition(piece, p, 1))
+                .filter(p -> ControlsUtility.checkPosition(piece, p, board))
+                .filter(p -> !ControlsUtility.checkPiece(piece, p, board) || ControlsUtility.checkEnemy(piece, p, board))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private boolean isMovementValid(final Piece piece, final int length, final Position direction, final Chessboard board) {
+        final Position p = ControlsUtility.getNewPosition(piece, direction, length);
+        if (!ControlsUtility.checkPosition(piece, p, board)) {
+            return false;
+        }
+        final Position previousPosition = ControlsUtility.getNewPosition(piece, direction, length - 1);
+
+        if (ControlsUtility.checkEnemy(piece, previousPosition, board)) {
+            return false;
+        }
+        return !(ControlsUtility.checkPiece(piece, p, board) && !ControlsUtility.checkEnemy(piece, p, board));
     }
 }
