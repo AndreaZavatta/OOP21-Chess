e7c857a9aefe0fcb5da420060f96be568c722d65
Andrea Zavatta <zavattaandrea@gmail.com>
2022-05-24 17:20:37 +0200

    add test

--- src/test/java/chessparsers/test/FenBuilderTest.java
+++ src/test/java/chessparsers/test/FenBuilderTest.java
@@ -28,24 +28,40 @@ class FenBuilderTest {
     void testInitialPosition() {
         assertEquals("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 
                 fenBuilder.halfMoveClock(0)
-                .fullMoveNumber(1)
                 .activeColor(WHITE)
                 .build(boardFactory.createNormalCB()));
     }
     @Test
     void testFirstPosition() {
-        List<Piece> list = List.of(pieceFact.createPiece(ROOK, createNewPosition("a8"), BLACK),
-                                   pieceFact.createPiece(KING, createNewPosition("e8"), BLACK),
-                                   pieceFact.createPiece(BISHOP, createNewPosition("h8"), BLACK),
-                                   pieceFact.createPiece(ROOK, createNewPosition("a1"), WHITE),
-                                   pieceFact.createPiece(KING, createNewPosition("d2"), WHITE));
+        List<Piece> list = 
+                List.of(pieceFact.createPiece(ROOK, createNewPosition("a8"), BLACK),
+                pieceFact.createPiece(KING, createNewPosition("e8"), BLACK),
+                pieceFact.createPiece(BISHOP, createNewPosition("h8"), BLACK),
+                pieceFact.createPiece(ROOK, createNewPosition("a1"), WHITE),
+                pieceFact.createPiece(KING, createNewPosition("d2"), WHITE));
         var board = boardFactory.createTestCB(list);
         assertEquals("r3k2b/8/8/8/8/8/3K4/R7 w q - 0 1", 
-                fenBuilder.fullMoveNumber(1)
-                .activeColor(WHITE)
+                fenBuilder.activeColor(WHITE)
                 .blackCastledKingside()
                 .whiteCastledKingside()
                 .whiteCastledQueenside()
                 .build(board));
+    }
+    @Test
+    void testSecondPosition() {
+        List<Piece> list = 
+                List.of(pieceFact.createPiece(KING, createNewPosition("b5"), BLACK),
+                pieceFact.createPiece(PAWN, createNewPosition("a2"), BLACK),
+                pieceFact.createPiece(KING, createNewPosition("f2"), WHITE),
+                pieceFact.createPiece(PAWN, createNewPosition("h4"), WHITE));
+        var board = boardFactory.createTestCB(list);
+        assertEquals("8/8/8/1k6/7P/8/p4K2/8 b - - 0 1", 
+                fenBuilder.activeColor(BLACK)
+                .blackCastledKingside()
+                .blackCastledQueenside()
+                .whiteCastledKingside()
+                .whiteCastledQueenside()
+                .build(board));
+
     }
 }
