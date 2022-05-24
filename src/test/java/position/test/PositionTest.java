fa5ecbe8f98853b26b708b36842ca06d7b6e2bd6
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-05-24 09:56:33 +0200

    add test

--- src/test/java/position/test/PositionTest.java
+++ src/test/java/position/test/PositionTest.java
@@ -12,12 +12,13 @@ class PositionTest {
     @Test
     void test() {
         //final Position pos = new Position(1, 2);
-        final Position pos1 = new Position(1, 1);
+        final Position pos1 = Position.createNumericPosition(1, 1);
         final String a = "ciao";
         final Position pos = Position.createNewPosition("a8");
         final Position pos2 = Position.createNewPosition("e4");
         System.out.println(pos.getX() + " " +  pos.getY());
         System.out.println(pos2.getX() + " " + pos2.getY());
+        System.out.println(pos1.getX() + " " + pos1.getY());
         //assertFalse(pos1.equals(pos));
         assertNotEquals(pos1, a);
     }
