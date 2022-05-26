fcaec04d8f9680ebd898f6381373320b424b31b2
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2022-05-26 11:34:33 +0200

    add UserController

---
+++ src/main/java/user/UserController.java
@@ -0,0 +1,17 @@
+package user;
+
+import javafx.scene.image.Image;
+
+/**
+ * 
+ * generic interface of userControllerImpl.
+ *
+ */
+public interface UserController extends User {
+
+    /**
+     * 
+     * @return user image
+     */
+    Image getImage();
+}
