2897c826def606bae8cf1c52a2bfbd34d9bb2ab4
Angela Speranza <angela.speranza@studio.unibo.it>
2022-05-26 15:41:29 +0200

    update javadoc

--- src/main/java/board/Castling.java
+++ src/main/java/board/Castling.java
@@ -1,14 +1,1 @@
-package board;
-
-import pieces.Piece;
-
-/**
- * 
- * This interface handles all Castling opportunities
- * 
- */
-public interface Castling {
-
-    boolean canCastle(Chessboard chessboard, Piece king, int xPos);
-
-}
+package board;import pieces.Piece;/** * *  This interface models and handles all Castling controls. * */public interface Castling {    boolean canCastle(Chessboard chessboard, Piece king, int xPos);}
