10900cfe45dba6f9fa02b9537f4eb625edd25f96
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-05-26 13:03:04 +0200

    add castling

--- src/main/java/pieces/King.java
+++ src/main/java/pieces/King.java
@@ -1,19 +1,21 @@
 package pieces;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import board.Castling;
+import board.CastlingImpl;
 import board.Chessboard;
-import piece.utils.Position;
-import piece.utils.Side;
-import piece.utils.Name;
-import piece.utils.PieceDirections;
+import piece.utils.*;
+
 /**
  * A King class that extends AbstractPiece abstract class.
  *
  */
 public class King extends AbstractPiece {
 
+    private final Castling castle = new CastlingImpl();
     private static final int KING_VALUE = 0;
     /**
      * A King piece constructor.
@@ -27,13 +29,25 @@ public class King extends AbstractPiece {
 
     @Override
     public List<Position> getAllPossiblePositions(final Chessboard board) {
-        return Collections
-                .unmodifiableList(this.getBasicMoves().singleIteration(PieceDirections.KING_DIR, board, this));
+        final List<Position> list = new ArrayList<>(this.getBasicMoves().singleIteration(PieceDirections.KING_DIR, board, this));
+        //castle on left king side
+        if (castle.canCastle(board, this, Numbers.ZERO)){
+            list.add(castleKingPosition(-Numbers.TWO));
+        }
+        //castle on right king side
+        if(castle.canCastle(board, this, Numbers.SEVEN)){
+            list.add(castleKingPosition(Numbers.TWO));
+        }
+        return Collections.unmodifiableList(list);
     }
 
     @Override
     public int getValue() {
         return KING_VALUE;
+    }
+
+    private Position castleKingPosition(final int direction) {
+        return Position.createNumericPosition(this.getPosition().getX() + direction, this.getPosition().getY());
     }
 
 }
