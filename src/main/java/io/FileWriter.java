be140dc506b676fc4c6acf5a8ea22905d35abb29
Andrea Zavatta <zavattaandrea@gmail.com>
2022-05-26 14:39:28 +0200

    update filewriter javadoc

--- src/main/java/io/FileWriter.java
+++ src/main/java/io/FileWriter.java
@@ -11,8 +11,8 @@ public interface FileWriter<T extends Serializable> {
 
     /**
      * a method for writing to a file.
-     * @param obj
-     * @throws IOException
+     * @param obj the object to be written to file
+     * @throws IOException if writing to file is not done correctly
      */
         void writeFile(T obj) throws IOException;
 
