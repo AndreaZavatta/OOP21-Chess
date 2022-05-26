cf0609b47c4246022a47e555eb4abacd84ae4144
Andrea Zavatta <zavattaandrea@gmail.com>
2022-05-26 14:45:40 +0200

    update ControlCheck javadoc

--- src/main/java/board/ControlCheck.java
+++ src/main/java/board/ControlCheck.java
@@ -13,15 +13,15 @@ public interface ControlCheck {
      * It is illegal to move your king into check.
      * this method checks these illegal moves and removes them from the available moves
      * 
-     * @param chessboard
-     * @param piece
+     * @param chessboard The chessboard where the moves must be controlled
+     * @param piece the piece to which have to check the moves
      * @return List<Position>
      */
     List<Position> controlledMoves(Chessboard chessboard, Piece piece);
     /**
      * this method checks if the king is in check, according to the color.
-     * @param chessboard
-     * @param color
+     * @param chessboard The chessboard in which to check whether the king is in check
+     * @param color The color of the king to check if it is in check
      * @return boolean
      */
     boolean isInCheck(Chessboard chessboard, Side color);
