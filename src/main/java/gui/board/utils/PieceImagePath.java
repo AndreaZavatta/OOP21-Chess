e9615b93c5bd105afa550fa80514f4b97f4a8592
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2022-05-30 10:43:10 +0200

    add enum for image path

---
+++ src/main/java/gui/board/utils/PieceImagePath.java
@@ -0,0 +1,62 @@
+package gui.board.utils;
+
+import application.Start;
+
+public enum PieceImagePath {
+
+    /**
+     * Pawn piece path.
+     */
+    PAWN("blackPawn.png", "whitePawn.png"),
+
+    /**
+     * Queen piece path.
+     */
+    QUEEN("blackQueen.png", "whiteQueen.png"),
+
+    /**
+     * King piece path.
+     */
+    KING("blackKing.png", "whiteKing.png"),
+
+    /**
+     * Knight piece path.
+     */
+    KNIGHT("blackKnight.png", "whiteKnight.png"),
+
+    /**
+     * Bishop piece path.
+     */
+    BISHOP("blackBishop.png", "whiteBishop.png"),
+
+    /**
+     * Rook piece path.
+     */
+    ROOK("blackRook.png", "whiteRook.png");
+
+    private static final String BASIC_PATH = Start.SEPARATOR + "pieces" + Start.SEPARATOR + "images" + Start.SEPARATOR;
+
+    private final String black;
+    private final String white;
+
+    PieceImagePath(final String black, final String white) {
+        this.black = black;
+        this.white = white;
+    }
+
+    /**
+     * 
+     * @return get black image.
+     */
+    public String getBlackPath() {
+        return BASIC_PATH + black;
+    }
+
+    /**
+     * 
+     * @return get white image.
+     */
+    public String getWhitePath() {
+        return BASIC_PATH + white;
+    }
+}
