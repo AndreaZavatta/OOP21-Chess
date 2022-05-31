bdda348c58cbe9f57af8263a527d15947e181f58
Andrea Zavatta <zavattaandrea@gmail.com>
2022-05-31 18:10:39 +0200

    connected button with textarea with controller

--- src/main/java/controller/StatsController.java
+++ src/main/java/controller/StatsController.java
@@ -1,7 +1,16 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class StatsController {
+
+
+    @FXML
+    private TextArea txtAreaStats = new TextArea();
+
     public void showStats(){
-        System.out.println("here the stats!");
+        txtAreaStats.setText("here the stats");
     }
 }
