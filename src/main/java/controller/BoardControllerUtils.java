78a673119f78d94895a9565cae80072bf03cff7a
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-06-05 17:36:33 +0200

    add support methods for BoardController

--- src/main/java/controller/BoardControllerUtils.java
+++ src/main/java/controller/BoardControllerUtils.java
@@ -4,9 +4,12 @@ import java.util.Map;
 
 import game.Game;
 import javafx.scene.effect.BlurType;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.effect.InnerShadow;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import model.piece.utils.Name;
 import model.piece.utils.Numbers;
 import model.pieces.Piece;
@@ -21,9 +24,9 @@ public final class BoardControllerUtils {
 
     private BoardControllerUtils() { }
     /**
-     * 
-     * @param match
-     * @param mapGuiPieceToPiece
+     * A getter for the king of this turn.
+     * @param match the current match.
+     * @param mapGuiPieceToPiece the current map of GuiPiece-Piece.
      * @return the king of this turn.
      */
     public static GuiPiece getKingOfThisTurn(final Game match, final Map<GuiPiece, Piece> mapGuiPieceToPiece) {
@@ -37,9 +40,9 @@ public final class BoardControllerUtils {
                 .findFirst().get();
     }
     /**
-     * 
-     * @param match
-     * @param mapGuiPieceToPiece
+     * A getter for the king of the opposite turn.
+     * @param match the current match.
+     * @param mapGuiPieceToPiece the current map of GuiPiece-Piece.
      * @return the king of the other turn.
      */
     public static GuiPiece getKingOfTheOtherTurn(final Game match, final Map<GuiPiece, Piece> mapGuiPieceToPiece) {
@@ -53,9 +56,10 @@ public final class BoardControllerUtils {
                 .findFirst().get();
     }
     /**
-     * 
-     * @param color
-     * @param rectangle
+     * A setter for the effects of the rectangles.
+     * Also used when the king is on check to show it.
+     * @param color the color of the effects.
+     * @param rectangle the affected rectangle.
      */
     public static void setEffect(final Color color, final Rectangle rectangle) {
         final InnerShadow shadow = new InnerShadow();
@@ -67,15 +71,46 @@ public final class BoardControllerUtils {
     }
 
     /**
-     *
-     * @param rectangle
-     * @param match
-     * @param mapGuiPieceToPiece
+     * A method for removing the effects of the rectangles.
+     * If the king is on check, it's effect will not be removed.
+     * @param rectangle the rectangle.
+     * @param match the current match.
+     * @param mapGuiPieceToPiece the current map GuiPiece-Piece.
      */
     public static void removeEffect(final Rectangle rectangle, final Game match, final Map<GuiPiece, Piece> mapGuiPieceToPiece) {
         if (!rectangle.equals(BoardControllerUtils.getKingOfThisTurn(match, mapGuiPieceToPiece).getRectangle())
                 || !match.isInCheck()) {
             rectangle.setEffect(null);
         }
+    }
+    /**
+     * A method for setting effects to the image and name of the player who is moving the pieces.
+     * It colors the name red and lower the brightness of the other player's image.
+     * @param text the name of the player who is moving the pieces.
+     * @param image the image of the other player.
+     */
+    public static void setEffectPlayerTurn(final Text text, final ImageView image) {
+        text.setStyle("-fx-font: 20 arial;");
+        text.setFill(Color.RED);
+        final ColorAdjust colorAdjust = new ColorAdjust();
+        colorAdjust.setBrightness(-0.5);
+        image.setEffect(colorAdjust);
+    }
+    /**
+     * A method used to remove any effect from an image and a text.
+     * @param text the text you are removing the effect.
+     * @param image the image you are removing the effect.
+     */
+    public static void removeEffects(final Text text, final ImageView image) {
+        image.setEffect(null);
+        setTextOptions(text);
+    }
+    /**
+     * A setter for the color, font and dimension of the text.
+     * @param text the text.
+     */
+    public static void setTextOptions(final Text text) {
+        text.setStyle("-fx-font: 18 arial;");
+        text.setFill(Color.WHITE);
     }
 }
