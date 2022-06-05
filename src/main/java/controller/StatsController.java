6770da28c9a46b1af17c6b2fbdb01d052e30b810
Andrea Zavatta <zavattaandrea@gmail.com>
2022-06-06 00:18:27 +0200

    create first implementation for visualizing data in tableview

--- src/main/java/controller/StatsController.java
+++ src/main/java/controller/StatsController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import game.Game;
+import game.GameImpl;
 import io.JsonFileReader;
 import io.JsonFileReaderImpl;
 import javafx.collections.FXCollections;
@@ -13,16 +14,20 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import model.piece.utils.Side;
+import pair.Pair;
+import pair.Triple;
+import user.User;
+import user.UserImpl;
 
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.io.IOException;
 import java.net.URL;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -41,7 +46,14 @@ public class StatsController implements Initializable {
     @FXML
     private TextArea txtAreaStats = new TextArea();
     @FXML
-    private TableView<String> tableView = new TableView<>();
+    private TableColumn<User, String> firstPlayer;
+    @FXML
+    private TableColumn<User, String> secondPlayer;
+    @FXML
+    private TableColumn<Instant, String> date;
+    @FXML
+    private TableView<Triple<User, User, Instant>> tableView = new TableView<>();
+
 
 
     public void showStats(){
@@ -52,10 +64,29 @@ public class StatsController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        Game game = new GameImpl(new Pair<>(new UserImpl("andrea"), Side.BLACK), new Pair<>(new UserImpl("marco"), Side.WHITE));
+        Game game2 = new GameImpl(new Pair<>(new UserImpl("alessia"), Side.BLACK), new Pair<>(new UserImpl("martina"), Side.WHITE));
+        List<Game> games = List.of(game, game2);
+        System.out.println("dopo game");
+        firstPlayer.setCellValueFactory(new PropertyValueFactory<>("x"));
+        secondPlayer.setCellValueFactory(new PropertyValueFactory<>("y"));
+        date.setCellValueFactory(new PropertyValueFactory<>("z"));
+        System.out.println("dopo set cell");
+        tableView.setItems(observableList());
         txtFieldName.textProperty().addListener((observableValue, s, s2) -> {
             txtAreaStats.setText(s2);
         });
     }
+
+    private ObservableList<Triple<User,User,Instant>> observableList(){
+        System.out.println(List.of(new Triple<>(new UserImpl("andrea"), new UserImpl("romina"),Instant.parse("2017-02-03T10:37:30.00Z"))));
+        return  FXCollections.observableArrayList(
+                //games.stream().map(x -> new Triple<>(x.getUsers().getX(), x.getUsers().getY(), x.getStartDate())).collect(Collectors.toList())
+                List.of(new Triple<>(new UserImpl("andrea"), new UserImpl("romina"),Instant.parse("2017-02-03T10:37:30.00Z")))
+
+        );
+    }
+
 
     @FXML
     void backToMenu(final ActionEvent event) {
