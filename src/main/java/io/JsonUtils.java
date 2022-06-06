35cf4d20e36346692025d82567505213cfc8d47a
Andrea Zavatta <zavattaandrea@gmail.com>
2022-06-07 00:22:55 +0200

    add javadoc to class

--- src/main/java/io/JsonUtils.java
+++ src/main/java/io/JsonUtils.java
@@ -6,13 +6,24 @@ import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
-public class JsonUtils {
-    public static ObjectMapper getMapper(){
+import java.util.TimeZone;
+/**
+ * this class has utils methods for input/output classes. 
+ *
+ */
+public final class JsonUtils {
+    private JsonUtils() { }
+    /**
+     * 
+     * @return ObjectMapper for serializing and deserializing objects
+     */
+    public static ObjectMapper getMapper() {
         return new ObjectMapper()
                 .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
                 .registerModule(new Jdk8Module())
                 .registerModule(new ParameterNamesModule())
                 .registerModule(new JavaTimeModule())
-                .enable(SerializationFeature.INDENT_OUTPUT);
+                .enable(SerializationFeature.INDENT_OUTPUT)
+                .setTimeZone(TimeZone.getDefault());
     }
 }
