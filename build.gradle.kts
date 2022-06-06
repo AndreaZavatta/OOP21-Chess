876564eb3e1929143f86b644f74013e99e60d18c
Andrea Zavatta <zavattaandrea@gmail.com>
2022-06-06 02:21:49 +0200

    add module to hanfle time conversion

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.regex.Pattern.compile
+
 plugins {
     // Apply the java plugin to add support for Java
     java
@@ -52,6 +54,9 @@ dependencies {
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.13.3")
     // https://mvnrepository.com/artifact/com.fasterxml.jackson.module/jackson-module-parameter-names
     implementation("com.fasterxml.jackson.module:jackson-module-parameter-names:2.13.3")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.5.1")
+
+
 
     // and a dependency on vert.x
     implementation("io.vertx:vertx-core:3.5.3")
