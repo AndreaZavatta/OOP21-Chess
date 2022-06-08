b0045433a891d9efdf338e682ea013ef98251ec6
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2022-06-08 19:58:08 +0200

    add a control in nextMove

--- src/main/java/game/GameImpl.java
+++ src/main/java/game/GameImpl.java
@@ -56,6 +56,9 @@ public class GameImpl implements Game {
 
     @Override
     public void nextMove(final Position firstPos, final Position finalPos) {
+        if (isFinished) {
+            return;
+        }
         final Optional<Piece> attacker = chessboard.getPieceOnPosition(firstPos);
         if (checkIllegalArgument(attacker, firstPos, finalPos)) {
             throw new IllegalArgumentException();
