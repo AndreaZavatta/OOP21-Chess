02d820e33590ffe941eaa20cfbd648b24f5f33d5
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-06-08 16:06:44 +0200

    refactor entire class

--- src/main/java/controller/AbstractController.java
+++ src/main/java/controller/AbstractController.java
@@ -11,11 +11,14 @@ import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-
-public abstract class AbstractController {
+/**
+ * 
+ *
+ */
+public final class AbstractController {
     private final Alert alert = new Alert(Alert.AlertType.NONE);
 
-    void changePage(final Event event, final String string) {
+    private void changePage(final Event event, final String string) {
         try {
             final FXMLLoader loader = new FXMLLoader(getClass().getResource(string));
             buildWindowNodes(event, loader);
@@ -23,21 +26,25 @@ public abstract class AbstractController {
             showAlert("unable to open menu", ERROR);
         }
     }
-    @FXML
-    void backToMenu(final Event event){
+
+    /**
+     * @FXML
+     * @param event
+     */
+    public void backToMenu(final Event event) {
         changePage(event, "/layouts/MainMenu.fxml");
     }
 
-    void showAlert(String str, Alert.AlertType type) {
+    private void showAlert(final String str, final Alert.AlertType type) {
         alert.setAlertType(type);
         alert.setContentText("");
         alert.setHeaderText(str);
         alert.show();
     }
 
-    void buildWindowNodes(final Event event, final FXMLLoader loader) throws IOException {
-        Parent root = loader.load();
-        Stage stage = new Stage();
+    private void buildWindowNodes(final Event event, final FXMLLoader loader) throws IOException {
+        final Parent root = loader.load();
+        final Stage stage = new Stage();
         stage.setTitle("L.A.M.A. Chess");
         stage.setScene(new Scene(root));
         stage.show();
