5972480839d8973031a4eda4528d8ddf80edf2eb
Andrea Zavatta <zavattaandrea@gmail.com>
2022-06-08 17:09:09 +0200

    update javadoc of Triple class

--- src/main/java/tuple/Triple.java
+++ src/main/java/tuple/Triple.java
@@ -2,34 +2,77 @@ package tuple;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
-
+/**
+ * 
+ * A generic Triple.
+ *
+ * @param <T>
+ * @param <U>
+ * @param <V>
+ */
 public class Triple<T, U, V> {
 
     private final ObjectProperty<T> x;
     private final ObjectProperty<U> y;
     private final ObjectProperty<V> z;
 
-    public Triple(T first, U second, V third) {
+    /**
+     * 
+     * @param first
+     * @param second
+     * @param third
+     */
+    public Triple(final T first, final U second, final V third) {
         this.x = new SimpleObjectProperty<>(first);
         this.y = new SimpleObjectProperty<>(second);
         this.z = new SimpleObjectProperty<>(third);
     }
 
-    public T getFirst() { return x.get(); }
-    public U getSecond() { return y.get(); }
-    public V getThird() { return z.get(); }
+    /**
+     * 
+     * @return T
+     */
+    public T getFirst() {
+        return x.get();
+    }
+    /**
+     * 
+     * @return U
+     */
+    public U getSecond() {
+        return y.get();
+    }
+    /**
+     * 
+     * @return V
+     */
+    public V getThird() {
+        return z.get(); 
+    }
 
-    public ObjectProperty<T> xProperty(){
+    /**
+     * this method is used in the controller for recognize data.
+     * @return ObjectProperty that represent x element
+     */
+    public ObjectProperty<T> xProperty() {
         return x;
     }
-    public ObjectProperty<U> yProperty(){
+    /**
+     * this method is used in the controller for recognize data.
+     * @return ObjectProperty that represent y element
+     */
+    public ObjectProperty<U> yProperty() {
         return y;
     }
-    public ObjectProperty<V> zProperty(){
+    /**
+     * this method is used in the controller for recognize data.
+     * @return ObjectProperty that represent z element
+     */
+    public ObjectProperty<V> zProperty() {
         return z;
     }
     @Override
-    public String toString(){
+    public String toString() {
         return x.get() + " " + y.get() + " " + z.get();
     }
 }
