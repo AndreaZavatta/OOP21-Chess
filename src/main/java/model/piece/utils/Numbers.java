637349123bb9c58dff719d98eebeaa4c304d1d0c
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2022-06-13 14:29:50 +0200

    update javadoc

--- src/main/java/model/piece/utils/Numbers.java
+++ src/main/java/model/piece/utils/Numbers.java
@@ -5,35 +5,35 @@ package model.piece.utils;
  */
 public final class Numbers {
     /**
-     * the number 7.
+     * the number 7, corresponding to the number 8 on the board.
      */
     public static final int SEVEN = 7;
     /**
-     * the number 6.
+     * the number 6, corresponding to the number 7 on the board.
      */
     public static final int SIX = 6;
     /**
-     * the number 5.
+     * the number 5, corresponding to the number 6 on the board.
      */
     public static final int FIVE = 5;
     /**
-     * the number 4.
+     * the number 4, corresponding to the number 5 on the board.
      */
     public static final int FOUR = 4;
     /**
-     * the number 3.
+     * the number 3, corresponding to the number 4 on the board.
      */
     public static final int THREE = 3;
     /**
-     * the number 2.
+     * the number 2, corresponding to the number 3 on the board.
      */
     public static final int TWO = 2;
     /**
-     * the number 1.
+     * the number 1, corresponding to the number 2 on the board.
      */
     public static final int ONE = 1;
     /**
-     * the number 0.
+     * the number 0, corresponding to the number 1 on the board.
      */
     public static final int ZERO = 0;
 
