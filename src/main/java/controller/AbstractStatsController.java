ec904d68d0265610184472705ffdd4a0f1bf2fce
Andrea Zavatta <zavattaandrea@gmail.com>
2022-06-19 01:41:47 +0200

    create AbstractStatsController to handle different controller implementations

---
+++ src/main/java/controller/AbstractStatsController.java
@@ -0,0 +1,67 @@
+package controller;
+
+import io.JsonFileReader;
+import io.JsonFileReaderImpl;
+import model.database.DatabaseFilters;
+import model.game.Game;
+import tuple.Triple;
+import user.User;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static javafx.scene.control.Alert.AlertType.ERROR;
+
+public class AbstractStatsController {
+    private final JsonFileReader fr = new JsonFileReaderImpl("database.txt");
+    DatabaseFilters database;
+    String helpMessage(){
+        return "You can use the text field above the table to filter "
+                + "matches by player's name.\n"
+                + "The search also allows you to see information such as percentages of "
+                + "games won, lost, and drawn of a given player.\n"
+                + "You can select a match to see the statistics associated with it";
+    }
+
+    Optional<User> getUserFromString(String input) {
+        final Optional<User> user = database.getUser(input);
+        return user.isEmpty() ? database.getFirstOccurrenceUser(input) : user;
+    }
+
+    private String getStatsSupport(final User user){
+        final long gameWon = database.getNumberGameWon(user);
+        final long gamePlayed = database.getNumberGamePlayed(user);
+        final long gameDraw = database.getNumberGameDrawn(user);
+        return  "Name: " + user.getName() + "\n"
+                + user.getName() + " won " + (gameWon * 100 / gamePlayed) + "% of game played\n"
+                + user.getName() + " draw " + (gameDraw * 100 / gamePlayed) + "% of game played\n"
+                + user.getName() + " lose " + ((gamePlayed - gameDraw - gameWon) * 100 / gamePlayed)
+                + "% of game played";
+    }
+
+    String getStats(final String str){
+        return getUserFromString(str)
+                .map(this::getStatsSupport).orElse("name not found");
+    }
+
+    String statsGamePresent(final String winner){
+        return winner.isEmpty() ? "the game ended in a draw" : "the winner is: "+winner;
+    }
+    String getWinner(final Triple<User, User, LocalDate> newSelection) {
+        return database.getGame(newSelection).isPresent() ? database.getWinner(newSelection) : "error! game not found";
+    }
+    List<Game> handleRead(Runnable runnable) {
+        List<Game> games = null;
+        try {
+            games = fr.readFile();
+        } catch (IOException e) {
+            runnable.run();
+
+        }
+        return games;
+    }
+
+
+}
--- src/main/java/controller/StatsController.java
+++ src/main/java/controller/StatsController.java
@@ -2,8 +2,6 @@ package controller;
 import static javafx.scene.control.Alert.AlertType.ERROR;
 import static javafx.scene.control.Alert.AlertType.INFORMATION;
 import model.game.Game;
-import io.JsonFileReader;
-import io.JsonFileReaderImpl;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
@@ -19,11 +17,10 @@ import javafx.scene.input.MouseEvent;
 import model.database.DatabaseFilters;
 import tuple.Triple;
 import user.User;
-import java.io.IOException;
+
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.function.Predicate;
 
@@ -31,9 +28,8 @@ import java.util.function.Predicate;
  * controller for updating stats view.
  *
  */
-public class StatsController implements Initializable {
+public class StatsController extends AbstractStatsController implements Initializable {
     private final ControllerUtils contr = new ControllerUtils();
-    private final JsonFileReader fr = new JsonFileReaderImpl("database.txt");
     @FXML
     private TextField txtFieldName = new TextField();
     @FXML
@@ -46,37 +42,14 @@ public class StatsController implements Initializable {
     private TableColumn<LocalDate, String> date;
     @FXML
     private TableView<Triple<User, User, LocalDate>> tableView = new TableView<>();
-    private DatabaseFilters database;
 
 
     @FXML
     public void showHelp() {
-        String str =    "You can use the text field above the table to filter "
-                        + "matches by player's name.\n"
-                        + "The search also allows you to see information such as percentages of "
-                        + "games won, lost, and drawn of a given player.\n"
-                        + "You can select a match to see the statistics associated with it";
-        contr.showCompleteAlert("Guide", "DATABASE GUIDE", str, INFORMATION);
+        contr.showCompleteAlert("Guide", "DATABASE GUIDE", helpMessage(), INFORMATION);
     }
     private void showStats() {
-        getUser().ifPresentOrElse(this::writeStats, () -> txtAreaStats.setText("name not found!"));
-    }
-
-    private Optional<User> getUser() {
-        final Optional<User> user = database.getUser(txtFieldName.getText());
-        return user.isEmpty() ? database.getFirstOccurrenceUser(txtFieldName.getText()) : user;
-    }
-
-    private void writeStats(final User user) {
-        final long gameWon = database.getNumberGameWon(user);
-        final long gamePlayed = database.getNumberGamePlayed(user);
-        final long gameDraw = database.getNumberGameDrawn(user);
-
-        txtAreaStats.setText("Name: " + user.getName() + "\n");
-        txtAreaStats.appendText(user.getName() + " won " + (gameWon * 100 / gamePlayed) + "% of game played\n");
-        txtAreaStats.appendText(user.getName() + " draw " + (gameDraw * 100 / gamePlayed) + "% of game played\n");
-        txtAreaStats.appendText(user.getName() + " lose " + ((gamePlayed - gameDraw - gameWon) * 100 / gamePlayed)
-                + "% of game played");
+        txtAreaStats.setText(getStats(txtFieldName.getText()));
     }
 
     @Override
@@ -104,18 +77,15 @@ public class StatsController implements Initializable {
     private void initializeTable() {
         tableView.setItems(observableList(x -> true));
         tableView.getSelectionModel()
-                .selectedItemProperty().addListener((obs, oldSelection, newSelection) -> writeWinner(newSelection));
+                .selectedItemProperty()
+                .addListener((obs, oldSelection, newSelection) -> txtAreaStats.setText(getWinner(newSelection)));
         tableView.setRowFactory(tableView2 -> addDeselectionRowEvent());
     }
 
     private List<Game> wrappedRead() {
-        List<Game> games = null;
-        try {
-            games = fr.readFile();
-        } catch (IOException e) {
-            contr.showCompleteAlert("Something went wrong", "error!", "unable to read database", ERROR);
-        }
-        return games;
+        return handleRead(
+                () -> contr.showCompleteAlert(
+                        "Something went wrong", "error!", "unable to read database", ERROR));
     }
     private Predicate<Game> filterByName(final String s2) {
         return x -> x.getUsers().getX().getName().contains(s2) || x.getUsers().getY().getName().contains(s2);
@@ -123,18 +93,7 @@ public class StatsController implements Initializable {
     private ObservableList<Triple<User, User, LocalDate>> observableList(final Predicate<Game> predicate) {
         return  FXCollections.observableArrayList(database.getTripleFromGame(predicate));
     }
-    private void writeWinner(final Triple<User, User, LocalDate> newSelection) {
-            database.getGame(newSelection).
-                    ifPresentOrElse(x -> writeStatsGamePresent(database.getWinner(newSelection)),
-                            () -> txtAreaStats.setText("error! game not found"));
-    }
-    private void writeStatsGamePresent(final String winner) {
-        if (winner.isEmpty()) {
-            txtAreaStats.setText("the game ended in a draw");
-        } else {
-            txtAreaStats.setText("the winner is: " + winner);
-        }
-    }
+
     private TableRow<Triple<User, User, LocalDate>> addDeselectionRowEvent() {
         final TableRow<Triple<User, User, LocalDate>> row = new TableRow<>();
         row.addEventFilter(MouseEvent.MOUSE_PRESSED, event -> deselectRow(row, event));
