852837e141e8c4408095ff5c8a4f5535f45031e5
Andrea Zavatta <zavattaandrea@gmail.com>
2022-06-21 17:27:53 +0200

    update tests

--- src/test/java/controls/test/ControlCheckTest.java
+++ src/test/java/controls/test/ControlCheckTest.java
@@ -42,81 +42,56 @@ class ControlCheckTest {
     }
     @Test
     void isInCheckTrue() {
-        final Piece blackKing = PIECE_FACTORY.createPiece(KING, createNewPosition("a8"), BLACK);
-        final Piece whiteRook = PIECE_FACTORY.createPiece(ROOK, createNewPosition("e8"), WHITE);
-        final Piece whiteKing = PIECE_FACTORY.createPiece(KING, createNewPosition("f4"), WHITE);
-        chessboard = CHESSBOARD_FACTORY.createTestCB(List.of(blackKing, whiteRook, whiteKing));
+        chessboard = CHESSBOARD_FACTORY.createCBToFen("k3R3/8/8/8/5K2/8/8/8 w - - 0 1");
         assertTrue(CONTROL.isInCheck(chessboard, BLACK));
     }
     @Test
     void beInCheckEnemyAttackKingTrue() {
-        final Piece blackKing = PIECE_FACTORY.createPiece(KING, createNewPosition("a8"), BLACK);
-        final Piece whiteRook = PIECE_FACTORY.createPiece(ROOK, createNewPosition("e8"), WHITE);
-        final Piece whiteKing = PIECE_FACTORY.createPiece(KING, createNewPosition("f4"), WHITE);
-        chessboard = CHESSBOARD_FACTORY.createTestCB(List.of(blackKing, whiteRook, whiteKing));
+        chessboard = CHESSBOARD_FACTORY.createCBToFen("k3R3/8/8/8/5K2/8/8/8 w - - 0 1");
         assertTrue(CONTROL.isInCheck(chessboard, BLACK));
         assertFalse(CONTROL.isInCheck(chessboard, WHITE));
     }
     @Test
     void ableToMovePinOnKingFalse() {
-        final Piece blackKing = PIECE_FACTORY.createPiece(KING, createNewPosition("a8"), BLACK);
-        final Piece blackBishop = PIECE_FACTORY.createPiece(BISHOP, createNewPosition("d8"), BLACK);
-        final Piece whiteRook = PIECE_FACTORY.createPiece(ROOK, createNewPosition("e8"), WHITE);
-        chessboard = CHESSBOARD_FACTORY.createTestCB(List.of(blackKing, blackBishop, whiteRook));
-        final List<Position> pos = CONTROL.controlledMoves(chessboard, blackBishop);
+        chessboard = CHESSBOARD_FACTORY.createCBToFen("k2bR3/8/8/8/5K2/8/8/8 b - - 0 1");
+        final List<Position> pos = CONTROL.controlledMoves(chessboard, chessboard.getPieceOnPosition(createNewPosition("d8")).get());
         assertEquals(pos, List.of());
     }
     @Test
     void ableToMoveNoPinOnKingTrue() {
-        final Piece blackKing = PIECE_FACTORY.createPiece(KING, createNewPosition("a8"), BLACK);
-        final Piece blackBishop = PIECE_FACTORY.createPiece(BISHOP, createNewPosition("d8"), BLACK);
-        final Piece whiteRook = PIECE_FACTORY.createPiece(ROOK, createNewPosition("e6"), WHITE);
-        chessboard = CHESSBOARD_FACTORY.createTestCB(List.of(blackKing, blackBishop, whiteRook));
-        final List<Position> pos = CONTROL.controlledMoves(chessboard, blackBishop);
+        chessboard = CHESSBOARD_FACTORY.createCBToFen("k2b4/8/4R3/8/5K2/8/8/8 b - - 0 1");
+        final List<Position> pos = CONTROL.controlledMoves(chessboard, chessboard.getPieceOnPosition(createNewPosition("d8")).get());
+        System.out.println(pos);
         assertTrue(pos.containsAll(List.of(createNewPosition("f6"), createNewPosition("e7"), createNewPosition("g5"), createNewPosition("h4"), createNewPosition("c7"), createNewPosition("b6"), createNewPosition("a5"))));
     }
     @Test
     void ableToMoveOnlyToCoverCheckTrue() {
-        final Piece blackKing = PIECE_FACTORY.createPiece(KING, createNewPosition("a8"), BLACK);
-        final Piece blackRook = PIECE_FACTORY.createPiece(ROOK, createNewPosition("e7"), BLACK);
-        final Piece whiteRook = PIECE_FACTORY.createPiece(ROOK, createNewPosition("a6"), WHITE);
-        chessboard = CHESSBOARD_FACTORY.createTestCB(List.of(blackKing, blackRook, whiteRook));
-        final List<Position> pos = CONTROL.controlledMoves(chessboard, blackRook);
+        chessboard = CHESSBOARD_FACTORY.createCBToFen("k7/4r3/R7/8/5K2/8/8/8 w - - 0 1");
+        final List<Position> pos = CONTROL.controlledMoves(chessboard, chessboard.getPieceOnPosition(createNewPosition("e7")).get());
         assertEquals(pos, List.of(createNewPosition("a7")));
     }
     @Test
     void ableToMoveOnlyToEscapeFromCheckTrue() {
-        final Piece blackKing = PIECE_FACTORY.createPiece(KING, createNewPosition("a8"), BLACK);
-        final Piece whiteRook = PIECE_FACTORY.createPiece(ROOK, createNewPosition("a6"), WHITE);
-        chessboard = CHESSBOARD_FACTORY.createTestCB(List.of(blackKing, whiteRook));
-        final List<Position> pos = CONTROL.controlledMoves(chessboard, blackKing);
+        chessboard = CHESSBOARD_FACTORY.createCBToFen("k7/8/R7/8/5K2/8/8/8 w - - 0 1");
+        final List<Position> pos = CONTROL.controlledMoves(chessboard, chessboard.getPieceOnPosition(createNewPosition("a8")).get());
         assertTrue(pos.containsAll(List.of(createNewPosition("b7"), createNewPosition("b8"))));
     }
     @Test
     void ableToMoveToCoverCheckTrue() {
-        final Piece blackKing = PIECE_FACTORY.createPiece(KING, createNewPosition("c6"), BLACK);
-        final Piece blackKnight = PIECE_FACTORY.createPiece(KNIGHT, createNewPosition("e3"), BLACK);
-        final Piece whiteRook = PIECE_FACTORY.createPiece(ROOK, createNewPosition("c3"), WHITE);
-        chessboard = CHESSBOARD_FACTORY.createTestCB(List.of(blackKing, blackKnight, whiteRook));
-        final List<Position> pos = CONTROL.controlledMoves(chessboard, blackKnight);
+        chessboard = CHESSBOARD_FACTORY.createCBToFen("8/8/2k5/6K1/8/2R1n3/8/8 b - - 0 1");
+        final List<Position> pos = CONTROL.controlledMoves(chessboard, chessboard.getPieceOnPosition(createNewPosition("e3")).get());
         assertEquals(pos, List.of(createNewPosition("c4")));
     }
     @Test
     void ableToMoveToCoverCheckOrToEatTrue() {
-        final Piece blackKing = PIECE_FACTORY.createPiece(KING, createNewPosition("c6"), BLACK);
-        final Piece blackKnight = PIECE_FACTORY.createPiece(KNIGHT, createNewPosition("e4"), BLACK);
-        final Piece whiteRook = PIECE_FACTORY.createPiece(ROOK, createNewPosition("c3"), WHITE);
-        chessboard = CHESSBOARD_FACTORY.createTestCB(List.of(blackKing, blackKnight, whiteRook));
-        final List<Position> pos = CONTROL.controlledMoves(chessboard, blackKnight);
+        chessboard = CHESSBOARD_FACTORY.createCBToFen("8/8/2k5/6K1/4n3/2R5/8/8 w - - 0 1");
+        final List<Position> pos = CONTROL.controlledMoves(chessboard, chessboard.getPieceOnPosition(createNewPosition("e4")).get());
         assertTrue(pos.containsAll(List.of(createNewPosition("c5"), createNewPosition("c3"))));
     }
     @Test
     void ableToMoveOnlyVerticallyTrue() {
-        final Piece blackKing = PIECE_FACTORY.createPiece(KING, createNewPosition("c7"), BLACK);
-        final Piece blackQueen = PIECE_FACTORY.createPiece(QUEEN, createNewPosition("c5"), BLACK);
-        final Piece whiteRook = PIECE_FACTORY.createPiece(ROOK, createNewPosition("c3"), WHITE);
-        chessboard = CHESSBOARD_FACTORY.createTestCB(List.of(blackKing, blackQueen, whiteRook));
-        final List<Position> pos = CONTROL.controlledMoves(chessboard, blackQueen);
+        chessboard = CHESSBOARD_FACTORY.createCBToFen("8/2k5/8/2q3K1/8/2R5/8/8 w - - 0 1");
+        final List<Position> pos = CONTROL.controlledMoves(chessboard, chessboard.getPieceOnPosition(createNewPosition("c5")).get());
         assertTrue(pos.containsAll(List.of(createNewPosition("c6"), createNewPosition("c3"), createNewPosition("c4"))));
     }
 
