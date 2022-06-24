bd6e4b162918c7a3aec3e0b01b77cacb3f83e00c
Raggini Marco <93598699+MarkRagg@users.noreply.github.com>
2022-06-24 15:08:02 +0200

    update javadoc

--- src/main/java/view/GuiPieceFactory.java
+++ src/main/java/view/GuiPieceFactory.java
@@ -12,7 +12,7 @@ public interface GuiPieceFactory {
     /**
      * It create a guiPiece from an input piece.
      * It's useful for view and controller.
-     * @param piece
+     * @param piece piece of the chessboard
      * @return a guiPiece of the input piece
      */
     GuiPiece createGuiPiece(Piece piece);
